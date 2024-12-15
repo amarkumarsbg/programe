f4a091d96e4518070210b1708cd1ed35685baa36
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-12-15 18:24:20 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+//racecar--> racecar
+//madam --> madam
+
+function isPalindrome(str) {
+  const reversedStr = str.split("").reverse().join("");
+
+  return str === reversedStr;
+}
+
+console.log(isPalindrome("madam"));
