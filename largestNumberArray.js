b203c83433758cabd276a1c1eb8cd69b10f9fa6b
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-12-15 18:44:27 +0530

    largestNumberArray

---
+++ largestNumberArray.js
@@ -0,0 +1,15 @@
+//find largest number in an array
+
+function largestNumber(arr) {
+  let largest = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+
+  return largest;
+}
+
+console.log(largestNumber([2, 4, 6, 7, 8, 9, 15]));
