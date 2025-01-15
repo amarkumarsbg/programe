0a68de887a8a8587ea57b4eff9ffc7546fcfb7f2
Amar Kumar <amarkumarsbg13@gmail.com>
2025-01-15 16:01:27 +0530

    changes in anagrams

--- coding/anagrams.js
+++ coding/anagrams.js
@@ -3,11 +3,20 @@
 // Dormitory → Dirty room
 // Elbow → Below
 
-function areAnagram(str1, str2) {
-  const sortedStr1 = str1.split("").sort().join("");
-  const sortedStr2 = str2.split("").sort().join("");
+// function areAnagram(str1, str2) {
+//   const sortedStr1 = str1.split("").sort().join("");
+//   const sortedStr2 = str2.split("").sort().join("");
 
-  return sortedStr1 === sortedStr2;
+//   return sortedStr1 === sortedStr2;
+// }
+
+//If you want to handle anagrams in a case-insensitive manner, you could update your function like this:
+//console.log(areAnagram("Dormitory", "Dirtyroom"));
+
+function areAnagram(str1, str2) {
+  const formatString = (str) => str.toLowerCase().split("").sort().join(""); // Convert both to lowercase
+
+  return formatString(str1) === formatString(str2);
 }
 
-console.log(areAnagram("Dormitory", "Dirtyroom"));
+console.log(areAnagram("silent", "listen")); // true
